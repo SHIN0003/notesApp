2aa2f15dcd0ed792570b4480663d6bac36b7d5d5
Andrew Tai <94415160+SHIN0003@users.noreply.github.com>
2022-08-27 14:02:16 -0400

    part 11

--- app/src/main/java/com/example/notesapp/Notesactivity.java
+++ app/src/main/java/com/example/notesapp/Notesactivity.java
@@ -2,12 +2,15 @@ package com.example.notesapp;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,7 +18,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
@@ -25,7 +30,10 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.Random;
 
 public class Notesactivity extends AppCompatActivity {
 
@@ -56,11 +64,46 @@ public class Notesactivity extends AppCompatActivity {
         Query query = firebaseFirestore.collection("notes").document(firebaseUser.getUid()).collection("myNotes").orderBy("title", Query.Direction.ASCENDING);
         FirestoreRecyclerOptions<FirebaseModel> allusernotes = new FirestoreRecyclerOptions.Builder<FirebaseModel>().setQuery(query, FirebaseModel.class).build();
         noteAdapter = new FirestoreRecyclerAdapter<FirebaseModel, NoteViewHolder>(allusernotes) {
+            @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             protected void onBindViewHolder(@NonNull NoteViewHolder holder, int position, @NonNull FirebaseModel model) {
+
+
+                int colorcode = getRandomColor();
+                holder.mnote.setBackgroundColor(holder.itemView.getResources().getColor(colorcode, null));
                 ImageView popupbutton = holder.itemView.findViewById(R.id.menupopbutton);
                 holder.notetitle.setText(model.getTitle());
                 holder.notecontent.setText(model.getContent());
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //we have to open note detail activity
+                        Intent intent = new Intent(view.getContext(), NoteDetails.class);
+                        view.getContext().startActivity(intent);
+                    }
+                });
+                popupbutton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        PopupMenu popupMenu = new PopupMenu(view.getContext(), view);
+                        popupMenu.setGravity(Gravity.END);
+                        popupMenu.getMenu().add("Edit").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                            @Override
+                            public boolean onMenuItemClick(MenuItem menuItem) {
+                                Intent intent = new Intent(view.getContext(), EditNoteActivity.class);
+                                view.getContext().startActivity(intent);
+                                return false;
+                            }
+                        });
+                        popupMenu.getMenu().add("Delete").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                            @Override
+                            public boolean onMenuItemClick(MenuItem menuItem) {
+                                Toast.makeText(view.getContext(), "This note is deleted", Toast.LENGTH_SHORT).show();
+                                return false;
+                            }
+                        });
+                    }
+                });
 
             }
 
@@ -120,5 +163,24 @@ public class Notesactivity extends AppCompatActivity {
         if(noteAdapter != null) {
             noteAdapter.stopListening();
         }
+    }
+
+    private int getRandomColor() {
+        List<Integer> colorcode = new ArrayList<>();
+        colorcode.add(R.color.gray);
+        colorcode.add(R.color.pink);
+        colorcode.add(R.color.lightgreen);
+        colorcode.add(R.color.skyblue);
+        colorcode.add(R.color.color1);
+        colorcode.add(R.color.color2);
+        colorcode.add(R.color.color3);
+        colorcode.add(R.color.color4);
+        colorcode.add(R.color.color5);
+        colorcode.add(R.color.green);
+
+        Random random = new Random();
+        int number = random.nextInt(colorcode.size());
+        return colorcode.get(number);
+
     }
 }
